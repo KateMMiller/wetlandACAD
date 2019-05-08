15d0864ab973749cd67d286cf29f8d7cfbb557cd
KateMMiller <kathryn_miller@nps.gov>
2019-05-08 09:55:22 -0400

    Added hourly precip as secondary axis in hydrograph plot

--- R/plot_hydro_site_year.R
+++ R/plot_hydro_site_year.R
@@ -12,6 +12,7 @@
 #' @param df the name of the data frame
 #' @param yvar Quoted name of the water level field
 #' @param site Quoted name of the site for the title
+#' @param years years to plot
 #'
 #' @examples
 #' welld<-read.csv('./Analysis/FINAL_DATA/well_prec_data_2013-2018.csv')
@@ -23,14 +24,21 @@
 #'
 #' @export
 
-plot_hydro_site_year<-function(df,yvar,site){
-  df<-df %>% select(doy_h,yvar,year)
-  colnames(df)<-c('doy_h','WL','year')
+plot_hydro_site_year<-function(df, yvar, site, years=2013:2018){
+  df<-df %>% select(doy_h, yvar, year, lag.precip) %>% filter(year %in% years) %>% droplevels()
+  colnames(df)<-c('doy_h', 'WL', 'year', 'lag.precip')
   print(ggplot(df,aes(x=doy_h,y=WL,group=year))+
-          geom_line(col='black')+facet_wrap(~year,nrow=length(unique(df$year)))+
+          geom_line(col='black')+ geom_line(aes(x=doy_h,y=lag.precip*5-35, group=year),col='blue')+
+          facet_wrap(~year,nrow=length(unique(df$year)))+
           geom_hline(yintercept=0,col='brown')+theme_bw()+
-          theme(plot.title = element_text(hjust = 0.5))+
+          theme(plot.title = element_text(hjust = 0.5), panel.grid.minor = element_blank(),
+                panel.grid.major = element_blank(),
+                axis.text.y.right = element_text(color = 'blue'),
+                axis.title.y.right = element_text(color = 'blue'))+
           labs(title=site,y='Water Level (cm)',x='Date')+
           scale_x_continuous(breaks=c(121,152,182,213,244,274),
-                             labels=c('May-01','Jun-01','Jul-01','Aug-01','Sep-01','Oct-01')))
+                             labels=c('May-01','Jun-01','Jul-01','Aug-01','Sep-01','Oct-01'))+
+          scale_y_continuous(sec.axis = sec_axis(~., breaks=c(-35,-25,-15), name='Hourly Precip. (cm)\n',
+                                                 labels=c('0', '2', '4')))
+  )
 }
