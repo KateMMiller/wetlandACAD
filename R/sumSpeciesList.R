2514a782923a8a1b9e08453fc742272ec91fbb6e
KateMMiller <kathryn_miller@nps.gov>
2024-10-31 14:20:51 -0400

    added filters

--- R/sumSpeciesList.R
+++ R/sumSpeciesList.R
@@ -9,12 +9,31 @@
 #' @param years Numeric. Filter on sample year, ranging from 2012 to 2024. By default, all years are returned. Note that years 2011, 2016, 2021, and 5-year intervals of years thereafter are EPA NWCA sites that are not
 #' @param QAQC Logical. Include QAQC visits (TRUE) or drop QAQC visits (FALSE; default).
 #' @param species_type Character. Options are c("all", "native", "exotic")
+#' @param include_protected Logical. If TRUE, returns protected species. If FALSE (default), removes protected species from returned data frame.
 #'
-#' @return Returns a data frame with vegetation MMIs for each site.
+#' @return Returns a data frame with species lists for each site.
+#'
+#' @examples
+#' \dontrun{
+#' # import RAM data with protected records- Note that to include protected records
+#' # protected species records must be included
+#' importRAM(export_protected = T)
+#'
+#' # Compile species list for all sites and all species
+#' spp_all <- sumSpeciesList(include_protected = T)
+#'
+#' # Compile species list for all sites and only non-protected species
+#' spp_public <- sumSpeciesList(include_protected = T)
+#'
+#' # Compile for 2024 and exotic species only
+#' spp_exo_24 <- sumSpeciesList(years = 2024, species_type = "exotic")
+#'
+#' }
+#'
 #' @export
 
 sumSpeciesList <- function(site = "all", panel = 1:4, years = 2012:format(Sys.Date(), "%Y"),
-                           QAQC = FALSE, species_type = "all"){
+                           QAQC = FALSE, species_type = "all", include_protected = T){
 
   #---- Error Handling ----
   site_list <- paste0("R-", sprintf("%02d", 1:40))
@@ -25,28 +44,46 @@ sumSpeciesList <- function(site = "all", panel = 1:4, years = 2012:format(Sys.Da
   stopifnot(class(years) %in% c("numeric", "integer"), years >= 2012)
   stopifnot(class(QAQC) == "logical")
   species_type <- match.arg(species_type, c("all", "native", "exotic"))
+  stopifnot(class(include_protected) == "logical")
 
   #---- Compile Data ----
   env <- if(exists("VIEWS_RAM")){VIEWS_RAM} else {.GlobalEnv}
 
   spplist <- tryCatch(get("species_list", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
-                                                              "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name")],
+                                                              "limited_RAM", "TSN", "Latin_Name", "quad_freq")],
                       error = function(e){stop("The tbl_species_list table was not found. Please import wetland RAM views.")}
                       )
-  visit <- get("visits", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type", "limited_RAM",
-                                             "Bryophyte_Cover", "Invasive_Cover")]
+  visit <- get("visits", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type", "limited_RAM")]
   loc <- get("locations", envir = env)[,c("Code", "Location_ID", "Panel", "xCoordinate", "yCoordinate", "UTM_Zone")]
 
+  locev <- left_join(loc, visit, by = c("Code", "Location_ID", "Panel"))
 
+  spp_loc <- left_join(locev, spplist, by = c("Code", "Location_ID", "Panel", "Visit_ID",
+                                              "Visit_Type", "limited_RAM", "Date", "Year"))
+  spp_site <- filter(spp_loc, Code %in% site)
+  spp_year <- filter(spp_site, Year %in% years)
+  spp_panel <- filter(spp_year, Panel %in% panel)
+  spp_qaqc <- if(QAQC == FALSE){filter(spp_panel, Visit_Type == "VS")} else {spp_panel}
 
-  if(nrow(vmmi_qaqc) == 0){
+  plants <- VIEWS_RAM$tlu_Plant
+  spp_comb <- left_join(spp_qaqc, plants, by = c("TSN", "Latin_Name"))
+
+  # species_type: Character. Options are c("all", "native", "exotic")
+  spp_type <- switch(species_type,
+                     "all" = spp_comb,
+                     "native" = spp_comb |> filter(Exotic == FALSE),
+                     "exotic" = spp_comb |> filter(Exotic == TRUE))
+
+  spp_prot <- if(include_protected == TRUE){spp_type
+  } else {filter(spp_type, Protected_species == FALSE)}
+
+  if(nrow(spp_prot) == 0){
     stop("Arguments returned a data frame with no records. Be sure you specified RAM years, and not EPA NWCA years.")}
 
-  vmmi_final <- left_join(vmmi_qaqc, loc, by = c("Code", "Location_ID", "Panel")) |>
+  spp_final <- spp_prot |>
     select(Code, Location_ID, Visit_ID, Panel, xCoordinate, yCoordinate, UTM_Zone, Date, Year, Visit_Type,
-           limited_RAM, meanC, Bryophyte_Cover, Invasive_Cover, Cover_Tolerant = sum_cov_tol,
-           vmmi, vmmi_rating)
+           limited_RAM, TSN, Latin_Name, Common, quad_freq, PLANTS_Code, CoC_ME_ACAD, Invasive, Protected_species, Coef_wetness)
 
-  return(vmmi_final)
+  return(spp_final)
 
   }
