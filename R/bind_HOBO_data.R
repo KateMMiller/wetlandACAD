2dd6cc1769061a4c32f3ad7ea6e9e1fcc2aa7a79
KateMMiller <kathryn_miller@nps.gov>
2025-12-10 10:28:23 -0500

    updated for new loggers; improved help doc; reduced deps; updated code

--- R/bind_HOBO_data.R
+++ R/bind_HOBO_data.R
@@ -3,31 +3,31 @@
 #' @importFrom purrr list_rbind map
 #'
 #' @description This function imports individual raw HOBO well data files that were exported
-#' as csvs in the HOBO app. Using the specified path in the function and assuming file structure
-#' is such that the spring (October to April data) visit files are in a separate folder from fall
-#' visit files, and that each site's file name contains with its 4-letter code (e.g. Duck Pond
-#' Peatland's code is DUCK) as specified in the site_codes argument. The individual site data are
-#' row bound to match the raw water level table that originally was tbl_Water_Level in the MS Access
-#' RAM backend, but is now a stand-alone csv.
+#' as csvs in the HOBOconnect iOS app. Spring (October to April data) visit files should be in a
+#' separate folder from fall visit files, each site should have its own csv, and each site's file
+#' name should contain its 4-letter code (e.g. Duck Pond Peatland's code is DUCK) as specified
+#' in the site_codes argument. The individual site data are row bound to match the raw water
+#' level table that matches tbl_Water_Level in the MS Access RAM backend.
 #'
 #' This function generates a long format of the raw water level data to append to the existing water
-#' level table. By default, function imports all 8 sentinel sites by their code. Changing site_codes
+#' level table. By default, this function imports all 8 sentinel sites by their site code. Changing site_codes
 #' and well_id allows you to import differing number and names of sites. Note that each imported file
 #' must contain the following columns: # (Index column), Date-Time (EDT/EST),
 #' Differential Pressure (kPa), Absolute Pressure (kPa), Temperature (C), Water Level (m),
-#' Barometric Pressure (kPa). If this hard coding causes difficulty, will revise function to be more
-#' generic in future.
+#' Barometric Pressure (kPa). If the hard coding of column names causes difficulty, will revise
+#' function to be more generic in future.
 #'
 #' Note that this function has been updated to work with data collected by MX2001 water level loggers
 #' with built-in atmospheric pressure correction. Use package version 0.1.5 to compile water
 #' level for HOBO U20 Loggers with external atmospheric pressure correction.
 #'
-#' @param path Quoted path of the folder where the exported Hobo tables are located.
-#' @param export /code{TRUE} or /code{FALSE}. Export csv file to specified path. Defaults to /code{TRUE}.
+#' @param path Quoted path of the folder where the exported Hobo csvs for a single visit are located.
+#' @param export /code{TRUE} or /code{FALSE}. If TRUE (Default), exports final csv file to specified path.
 #' @param site_codes Quoted string of 4-letter site codes which are assumed to be in the file names
 #' of the water level csvs to be imported. If importing all 8 sentinel sites without changes, default
 #' settings do not need to be changed. If sites differ from default settings, in length or name, then both
-#' site_codes and well_id must be specified and match in length.
+#' site_codes and well_id must be specified and match in length. Function only accepts one file per site code
+#' in specified path.
 #' @param well_id Numbered list of Well_ID matching tbl_Well in MS Access RAM database. If importing
 #' all 8 sentinel sites without changes, default settings do not need to be changed. This is the primary
 #' key used to link water level data to well visit data tables. The well_id should match the Well_ID record
